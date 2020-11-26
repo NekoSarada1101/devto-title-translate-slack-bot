0f597a803b241d5ba66361e69de07c0187815b67
Sarada1101 <ryoyukikazu11@gmail.com>
2020-11-26 18:11:00 +0900

    feat(main.py): 取得した記事を一つずつ整形

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import feedparser
+from settings_secret import *
 
 
 def fetch_rss():
     dic = feedparser.parse(RSS_URL)  # type: dict
+    for i, entry in enumerate(dic.entries):
+        text = entry.title  # type: str
+        if i == 0 or "RT by @ThePracticalDev" in text:
+            continue
+        # text内のurlを取得
+        url = text[text.find("https://dev.to/"):len(text)]  # type: str
+        # 余計な部分を削除する
+        text = text.replace("#DEVCommunity", "")
+        text = text.replace(text[text.find("{ author"):text.rfind("}") + 1], "")
+        text = text.replace(url, "")
 
 
 if __name__ == '__main__':
